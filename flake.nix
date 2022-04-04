24bf557862e3d01285e02173cd5e712d1fd441cb
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2022-04-04 22:10:31 +0200

    Move nixify plugin to composer.local.json; add upgrade script

--- flake.nix
+++ flake.nix
@@ -8,6 +8,14 @@
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
       composer = pkgs.phpPackages.composer;
+      upgrade = pkgs.writeScript "mw-upgrade" ''
+        if [[ $# -ne 1 ]]; then
+          echo "usage: $0 <RELEASE_BRANCH>"
+          exit 1
+        fi
+        ${pkgs.curl}/bin/curl https://raw.githubusercontent.com/wikimedia/mediawiki/$1/composer.json > composer.json
+        ${pkgs.git}/bin/git add composer.json
+      '';
       update = pkgs.writeScript "composer-update" ''
         ${composer}/bin/composer update --no-dev
       '';
@@ -21,8 +29,10 @@
       };
 
       apps.x86_64-linux = {
+        upgrade = { type = "app"; program = "${upgrade}"; };
         update = { type = "app"; program = "${update}"; };
         nixify = { type = "app"; program = "${nixify}"; };
+        composer = { type = "app"; program = "${composer}/bin/composer"; };
       };
       defaultApp.x86_64-linux = self.apps.x86_64-linux.nixify;
       devShell.x86_64-linux =
