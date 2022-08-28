99a9b87d721d32d6f856f92e235e337973ca950b
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2022-08-28 20:00:17 +0200

    Downgrade extensions to currently installed version

--- flake.nix
+++ flake.nix
@@ -1,12 +1,19 @@
 {
-  inputs = {nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";};
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    legacy-packages.url = "github:NixOS/nixpkgs/nixos-20.03";
+  };
 
   outputs = {
     self,
     nixpkgs,
+    legacy-packages,
   }: let
-    pkgs = import nixpkgs {system = "x86_64-linux";};
-    composer = pkgs.phpPackages.composer;
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs {inherit system;};
+    composer1 = (import legacy-packages {inherit system;}).phpPackages.composer;
+    composer2 = pkgs.phpPackages.composer;
     git = pkgs.git;
 
     upgradeScript = pkgs.writeShellScript "mw-upgrade" ''
@@ -17,14 +24,21 @@
         exit 1
       fi
 
+      COMPOSER=${composer2}
+      for legacy in "REL1_28"; do
+        if [[ "$1" == "$legacy" ]]; then
+          COMPOSER=${composer1}
+        fi
+      done
+
       echo "Upgrade mediawiki tree using upstream release branch \`$1'"
       ${git}/bin/git rm -rf --ignore-unmatch mediawiki
       rm -rf mediawiki
       ${git}/bin/git clone --depth 1 --recurse-submodules -b $1 -- https://github.com/wikimedia/mediawiki.git mediawiki
       cp composer.local.json mediawiki
       pushd mediawiki
-      ${composer}/bin/composer update --no-dev
-      ${composer}/bin/composer dump-autoload
+      $COMPOSER/bin/composer update --no-dev
+      $COMPOSER/bin/composer dump-autoload
       popd
       find mediawiki -name .git -exec rm -rf {} +
       ${git}/bin/git add mediawiki
@@ -33,8 +47,8 @@
     updateScript = pkgs.writeShellScript "composer-update" ''
       echo "Running \`composer update' in mediawiki tree"
       pushd mediawiki
-      ${composer}/bin/composer update --no-dev
-      ${composer}/bin/composer dump-autoloads
+      $COMPOSER/bin/composer update --no-dev
+      $COMPOSER/bin/composer dump-autoloads
       popd
       ${git}/bin/git add mediawiki
     '';
@@ -64,23 +78,29 @@
   in {
     overlay = final: prev: {inherit komapedia-mediawiki;};
 
-    apps.x86_64-linux = rec {
-      upgrade = {
-        type = "app";
-        program = "${upgradeScript}";
-      };
+    apps.x86_64-linux = let
       update = {
         type = "app";
         program = "${updateScript}";
       };
+      upgrade = {
+        type = "app";
+        program = "${upgradeScript}";
+      };
+    in {
+      inherit update upgrade;
       composer = {
         type = "app";
-        program = "${composer}/bin/composer";
+        program = "${composer2}/bin/composer";
+      };
+      composer1 = {
+        type = "app";
+        program = "${composer1}/bin/composer";
       };
       default = update;
     };
     devShell.x86_64-linux =
-      pkgs.mkShell {nativeBuildInputs = [composer];};
+      pkgs.mkShell {nativeBuildInputs = [composer1 composer2];};
     packages.x86_64-linux = {inherit komapedia-mediawiki;};
     nixosModules.komapedia = import ./modules/komapedia.nix;
     formatter.x86_64-linux = pkgs.alejandra;
