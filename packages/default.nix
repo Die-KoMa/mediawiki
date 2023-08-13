77d8f8d96a2413dc0df17683d22998a6cdee0407
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2023-08-13 18:21:20 +0200

    Also substitute paths to other extensions

--- packages/default.nix
+++ packages/default.nix
@@ -18,13 +18,16 @@
     json = jsonForExtension "composer" name;
     dir = lib.replaceStrings ["/"] ["-"] json.name;
   in "share/php/${dir}";
-  composerExtension = name: let
+  composerExtension = name: composerExtension' name {};
+  composerExtension' = name: fixups: let
     drv = import (./. + "/${name}") {
       inherit pkgs system;
       noDev = true;
     };
     meta = metaForExtension name;
     path = pathForExtension name;
+
+    replacements = lib.concatStringsSep " " (lib.mapAttrsToList (name: path: ''--replace "__DIR__ . '/../..' . '/extensions/${name}/" "'${path}/"'') fixups);
   in
     pkgs.stdenv.mkDerivation {
       inherit (meta) pname version meta;
@@ -37,14 +40,14 @@
         cp -R ${drv}/${path}/vendor vendor
 
         pushd vendor/composer/
-        substituteInPlace *.json *.php --replace "/extensions/${name}/" "/"
+        substituteInPlace *.json *.php --replace "/extensions/${name}/" "/" ${replacements}
         popd
 
         cp -R vendor $out/
       '';
     };
-in {
+in rec {
   PageForms = composerExtension "PageForms";
   SemanticMediaWiki = composerExtension "SemanticMediaWiki";
-  SemanticResultFormats = composerExtension "SemanticResultFormats";
+  SemanticResultFormats = composerExtension' "SemanticResultFormats" {inherit SemanticMediaWiki;};
 }
