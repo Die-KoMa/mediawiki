38ac257a69b2a9fb08c2c491e746222ebdcddaa7
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2023-08-13 17:24:43 +0200

    Fix paths in packaged extensions

--- packages/default.nix
+++ packages/default.nix
@@ -4,10 +4,38 @@
   system,
   ...
 }: let
-  composerExtension = name:
-    import (./. + "/${name}") {
+  jsonForExtension = kind: name: builtins.fromJSON (builtins.readFile (./. + "/${name}/extensions/${name}/${kind}.json"));
+  metaForExtension = name: let
+    json = jsonForExtension "extension" name;
+  in {
+    pname = json.name;
+    inherit (json) version;
+    meta = lib.optionalAttrs (json.license-name == "GPL-2.0-or-later") {
+      license = lib.licenses.gpl2Plus;
+    };
+  };
+  pathForExtension = name: let
+    json = jsonForExtension "composer" name;
+    dir = lib.replaceStrings ["/"] ["-"] json.name;
+  in "share/php/${dir}";
+  composerExtension = name: let
+    drv = import (./. + "/${name}") {
       inherit pkgs system;
       noDev = true;
+    };
+    meta = metaForExtension name;
+    path = pathForExtension name;
+  in
+    pkgs.stdenv.mkDerivation {
+      inherit (meta) pname version meta;
+
+      dontUnpack = true;
+
+      installPhase = ''
+        mkdir $out
+        cp -R ${drv}/${path}/extensions/${name}/* $out
+        cp -R ${drv}/${path}/vendor $out/vendor
+      '';
     };
 in {
   PageForms = composerExtension "PageForms";
