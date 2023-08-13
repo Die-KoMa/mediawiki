44032515941a0ee54a7479b0a352878ba256c7a3
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2023-08-13 14:36:33 +0200

    Bump extensions to latest

--- packages/update-script.nix
+++ packages/update-script.nix
@@ -6,7 +6,7 @@
 writeShellScriptBin "composer-update" ''
   echo "Running \`composer update' in mediawiki tree"
   pushd mediawiki
-  ${composer}/bin/composer update --no-dev
+  ${composer}/bin/composer update --no-dev --optimize-autoloader
   ${composer}/bin/composer dump-autoload
   popd
   ${git}/bin/git add mediawiki
