888a04a47ccdfc776e2bd473939f1f9cbde8630c
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2023-12-17 16:02:09 +0100

    Vendor all extensions

--- packages/default.nix
+++ packages/default.nix
@@ -46,22 +46,20 @@
         cp -R vendor $out/
       '';
     };
-  extdistExtension = {
-    url,
-    hash,
-  }: let
-    filename = lib.removePrefix "https://extdist.wmflabs.org/dist/extensions/" url;
+  extdistExtension = src: let
+    components = lib.splitString "/" src;
+    filename = lib.last components;
     bare = lib.removeSuffix ".tar.gz" filename;
     parts = lib.splitString "-" bare;
     pname = lib.head parts;
     version = lib.concatStringsSep "-" (lib.tail parts);
   in
     pkgs.stdenv.mkDerivation {
-      inherit pname version;
-      src = pkgs.fetchzip {inherit url hash;};
+      inherit pname version src;
 
       installPhase = ''
-        cp -R $src $out
+        mkdir $out
+        tar --strip-components=1 --one-top-level=$out -xf $src
       '';
     };
 in rec {
@@ -69,20 +67,8 @@ in rec {
   SemanticMediaWiki = composerExtension "SemanticMediaWiki";
   SemanticResultFormats = composerExtension' "SemanticResultFormats" {inherit SemanticMediaWiki;};
 
-  EditSubpages = extdistExtension {
-    url = "https://extdist.wmflabs.org/dist/extensions/EditSubpages-REL1_39-e462ff9.tar.gz";
-    hash = "sha256-Q0sAaCaF4HOdspxGR97DL16i7WFGv1Ha//ToK/Sq+kc=";
-  };
-  UserMerge = extdistExtension {
-    url = "https://extdist.wmflabs.org/dist/extensions/UserMerge-REL1_39-89621f4.tar.gz";
-    hash = "sha256-+Xjh6YWnaBER8v1LOhQaWZNg2EMwkeZq8isPp9u9fBI=";
-  };
-  Variables = extdistExtension {
-    url = "https://extdist.wmflabs.org/dist/extensions/Variables-REL1_39-c1dea98.tar.gz";
-    hash = "sha256-iSnPt5VBE1vNRFHIJflmMIIMxsr/fzP1gdGpzQT/7xo=";
-  };
-  NativeSvgHandler = extdistExtension {
-    url = "https://extdist.wmflabs.org/dist/extensions/NativeSvgHandler-REL1_39-95310ed.tar.gz";
-    hash = "sha256-EUrHF1JBR3bc3kwwYv6rvPseG0rl/YyBp4GjI5q+ETo=";
-  };
+  EditSubpages = extdistExtension ./EditSubpages-REL1_39-e462ff9.tar.gz;
+  UserMerge = extdistExtension ./UserMerge-REL1_39-adcd648.tar.gz;
+  Variables = extdistExtension ./Variables-REL1_39-c1dea98.tar.gz;
+  NativeSvgHandler = extdistExtension ./NativeSvgHandler-REL1_39-95310ed.tar.gz;
 }
