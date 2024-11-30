aaf3f6b8c99941d1ef016d05ea661211affa0f59
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2024-11-30 14:32:55 +0100

    Bump to nixpkgs-24.11

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     composer2nix = {
       url = "github:svanderburg/composer2nix";
       flake = false;
